892fdabed6798d2a1fc556846d8acb7f091d8638
Nishant A S <nishantas4043@gmail.com>
2024-07-02 12:47:00 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/authenticator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.authenticator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/nish/authenticator/authenticator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nish.authenticator.authenticator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
